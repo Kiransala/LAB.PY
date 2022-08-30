307c0968784a8bd8f0d33468356d7dca53a8b51a
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-30 23:30:44 +0530

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -1,27 +1,42 @@
-list1 =[int(x) for x in input("Enter odd numbers : ").split()]
-list2 = [int(x) for x in input("Enter even numbers : ").split()]
+list1 =[int(x) for x in input("Enter odd numbers: ").split()]
+list2 = [int(x) for x in input("Enter even numbers: ").split()]
+print("Odd values: ", list1, "Even values: ", list2)
+        
+condition = False
 
-print("odd values ", list1 , "even values", list2 )
+while not condition:
+    print("\n")
+    choice = int(input("Choose one operation on lists: \n1.Merge and sort the two lists.\n2.Update the first element with X value and delete the middle element of the list.\n3.Find max and min elements from the list.\n4.Add N names into the existing number list and check if word python is present in the list.\n5.Exit.\nChoice: "))
+    if choice == 1:
+        list3 = list()
+        list3 = list1 + list2
+        print("Concatinated list is: ",list3)
+        list3.sort()
+        print("Sorted list is: ",list3)
+        
+    elif choice == 2:
+        list3[0]= int(input("Update the first element with "))
+        mid = len(list3)//2
+        list3.remove(list3[mid])
+        print(list3)
 
-list3 = list()
-list3 = list1 + list2
-print("orignal list is ",list3)
-list3.sort()
-print("sorted elemnents in list are ",list3)
-
-list3[0]= int(input("enter number at index 0 "))
-print(list3)
-
-mid = len(list3)//2
-list3.remove(list3[mid])
-print(list3)
-
-listn = [x for x in input("Names : ").split()]
-
-for n in listn:
-    list3.append(n)
-
-if list3.count("python")>0 :
-    print("present")
-else:
-    print("absent")
+    elif choice == 3:
+        maximum = max(list3)
+        minimum = min(list3)
+        print("Max = %d \nMin = %d\n" %(maximum,minimum))
+        
+    elif choice == 4:
+        listn = [x for x in input("Enter names : ").split()]
+    
+        for n in listn:
+            list3.append(n)
+    
+        if list3.count("python") > 0:
+            print("Python is present.\n")
+        else:
+            print("Python is absent.\n")
+        
+    elif choice == 5:
+        condition = True
+    else:
+        print("Enter a valid choice\n")
