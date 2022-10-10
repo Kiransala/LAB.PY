cf8af4739c2cd8f17314eb15b811f5f8206bb1aa
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-10-10 22:27:35 +0530

    Update exp9.py

--- exp9.py
+++ exp9.py
@@ -1,25 +1,46 @@
-from exp92 import subtraction 
+"""THIS IS 'main.py' FILE"""
+
+from exp92 import subtraction,multiply,divide
 from x.exp91 import addition
 
-print(addition(1,2,3))
-print(addition(2,3))
-print(addition(1))
+user = False
+while not user:
+    choice = int(
+        input("\n\n1.Addition\n2.Subtraction \n3.Mltiplication\n4.Division\n5.Exit\nEnter the choice: "))
+    if choice == 1:
+        n=int(input("No-of inputs: "))
+        if n == 3:
+          a=int(input("Enter a number a: "))
+          b=int(input("Enter a number b: "))
+          c=int(input("Enter a number c: "))
+          print(f"Answer for Addition: {addition(a,b,c)}")
+        elif n == 2:
+          a=int(input("Enter a number a: "))
+          b=int(input("Enter a number b: "))
+          print(f"Answer for Addition: {addition(a,b)}")
+        else:
+          print("Enter a valid choice")
 
-print(subtraction(3,2))
+    elif choice == 2:
+      a=int(input("Enter a number a: "))
+      b=int(input("Enter a number b: "))
+      print(f"Answer for Subtraction: {subtraction(a,b)}")
 
 
+    elif choice == 3:
+      a=int(input("Enter a number a: "))
+      b=int(input("Enter a number b: "))
+      print(f"Answer for Multiplication: {multiply(a,b)}")
+        
+    elif choice == 4:
+      a=int(input("Enter a number a: "))
+      b=int(input("Enter a number b: "))
+      print(f"Answer for Division: {divide(a,b)}")
 
-def addition(a=None,b=None,c=None):
-	if a!=None and b!=None and c!=None:
-		return a+b+c
-		
-	elif a!=None and b!=None:
-		return a+b
-		
-	else :
-		return a
-  
-  
-  def subtraction(a=None,b=None):
-	if a!=None and b!=None:
-		return a-b
+    elif choice == 5:
+      print("Exited")
+      user = True
+        
+    else:
+        print("Enter a valid choice")
+
