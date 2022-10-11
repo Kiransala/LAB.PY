4982c31fcb677db2a2715006d7b8b3f2c9cf393d
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-10-11 13:02:58 +0530

    Create exp10.py

---
+++ exp10.py
@@ -0,0 +1,28 @@
+import random
+
+class NumTooLargeError(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+class NumTooSmallError(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+    
+num = random.randint(1,100)
+count = 100
+while True:
+    count+=1
+    try:
+        inum=int(input("Enter a number: "))
+        if inum<num:
+            raise NumTooSmallError("Enter a Greater number")
+            
+        elif inum>num:
+            raise NumTooLargeError("Enter a Smaller number")
+        else:
+            break
+    except NumTooSmallError as me:
+        print(me)
+    except NumTooLargeError as me:
+        print(me)
+
+print(f"Correctly gussed the Number: {num} in Gusses: {num}")
