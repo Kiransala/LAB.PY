69531c16e723da7167c25b30c497f371cf32db16
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-11-14 21:56:04 +0530

    Update exp4.py

--- exp4.py
+++ exp4.py
@@ -1,36 +1,33 @@
-n = int(input("Enter the number of students: "))
+s = int(input("Enter the number off students: "))
 lst1 = list()
 
-for i in range(0, n):
-    r = int(input("Enter roll number: "))
-    n1 = input("Enter a name: ")
-    m1 = int(input("Enter marks of subject1: "))
-    m2 = int(input("Enter marks of subject2: "))
-    m3 = int(input("Enter marks of subject3: "))
-    l1 = [r, n1, m1, m2, m3]
+for i in range(0,s):
+    r = int(input("Enter the rollno: "))
+    n = input("Enter the name: ")
+    m1 = int(input("Marks of subject1: "))
+    m2 = int(input("Marks of subject2: "))
+    m3 = int(input("Marks of subject3: "))
+    l1 = [r, n, m1, m2, m3]
     tpl = tuple(l1)
     lst1.append(tpl)
     
 tpl = tuple(lst1)
 
-user = False
-while not user:
-    choice = int(
-        input("1.Print Tuple \n2.Tuple containing python \n3.Sorted tuple \n4.Exit\nEnter the choice: "))
+condition = False
+
+while not condition:
+    choice = int(input("\n\n1.Print Tuple \n2.Tuple containing python \n3.Sorted Tuple \n4.Exit \nEnter choice: "))
     if choice == 1:
-        print("Tuple: ")
-        print(tpl)
+        print(f"Tuple: {tpl}")
     elif choice == 2:
         print("Tuple containing python: ")
         for t in tpl:
             if "python" in t:
                 print(t)
     elif choice == 3:
-        print("Sorted tuple according to names: ")
+        print("Sorted tuples according to names: ")
         print(sorted(tpl, key=lambda x: x[1]))
-
     elif choice == 4:
-        print("Exited")
-        user = True
+        condition = True
     else:
-        print("Enter a valid choice")
+        print("Invalid choice.")
