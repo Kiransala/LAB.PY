b4b978305c21e437eecfb9006c3f196aa03dfc39
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-11-14 23:24:42 +0530

    Update exp5.py

--- exp5.py
+++ exp5.py
@@ -1,19 +1,20 @@
-s1={x for x in input("Enter string 1: ")}
-s2={x for x in input("Enter string 2: ")}
-print("String is: ", s1 ,"::", s2)
+s1 = {x for x in input("Enter String1: ")}
+s2 = {x for x in input("Enter String2: ")}
+print("String is: ", s1, "::", s2)
 
-user=False
-while not user:
-	choice=int(input("\n\n1.Display Common \n2.Display difference\n3.Display union\n4.Dispay Symmetric difference\n5.Exit\nEnter choice: "))
-	if choice==1 :
-		print(s1.intersection(s2))
-	elif choice==2 :
-		print(s1.difference(s2))
-	elif choice==3 :
-		print(s1.union(s2))
-	elif choice==4 :
-		print(s1.symmetric_difference(s2))
-	elif choice==5 :
-		user=True
-	else :
-		print("Invalid choice")
+condition = False
+
+while not condition:
+    choice = int(input("\n\n1.Display Common \n2.Display difference \n3.Display union \n4.Display symmetric difference \n5.Exit \nEnter choice: "))
+    if choice == 1:
+        print(s1.intersection(s2))
+    elif choice == 2:
+        print(s1.difference(s2))
+    elif choice == 3:
+        print(s1.union(s2))
+    elif choice == 4:
+        print(s1.symmetric_difference(s2))
+    elif choice == 5:
+        condition = True
+    else:
+        print("Invalid choice.")
