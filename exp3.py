5b2ffaf801c9bbc66b2f6537b64da307ed2b4d3e
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-11-14 20:36:44 +0530

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -1,21 +1,20 @@
-list1 =[int(x) for x in input("Enter odd numbers: ").split()]
-list2 = [int(x) for x in input("Enter even numbers: ").split()]
-print("Odd values: ", list1, "Even values: ", list2)
-        
+list1 = [int(x) for x in input("Enter odd values: ").split()]
+list2 = [int(x) for x in input("Enter even values: ").split()]
+print(f"Odd values: {list1} \nEven values: {list2}")
+
 condition = False
 
 while not condition:
-    print("\n")
-    choice = int(input("Choose one operation : \n1.Merge and sort the two lists.\n2.Update the first element with X value and delete the middle element of the list.\n3.Find max and min elements from the list.\n4.Add N names into the existing number list and check if word python is present in the list.\n5.Exit.\nChoice: "))
+    choice = int(input("\n\n1.Merge and Sort two list \n2.Update the first element x and delete the middle element\n3.Find max and min from the list\n4.Add elements into the list and check if python is present \n5.Exit\nEnter the choice: "))
     if choice == 1:
         list3 = list()
         list3 = list1 + list2
-        print("Concatinated list is: ",list3)
+        print(f"The merged list is: {list3}")
         list3.sort()
-        print("Sorted list is: ",list3)
-        
+        print(f"The sorted list is: {list3}")
+
     elif choice == 2:
-        list3[0]= int(input("Update the first element with "))
+        list3[0] = int(input("Enter the new value: "))
         mid = len(list3)//2
         list3.remove(list3[mid])
         print(list3)
@@ -23,20 +22,22 @@ while not condition:
     elif choice == 3:
         maximum = max(list3)
         minimum = min(list3)
-        print("Max = %d \nMin = %d\n" %(maximum,minimum))
-        
+        print(
+            f"The maximum value is {maximum} and minimum value is {minimum}.")
+
     elif choice == 4:
-        listn = [x for x in input("Enter names : ").split()]
-    
+        listn = [x for x in input("Enter the elements: ").split()]
+
         for n in listn:
             list3.append(n)
-    
+
         if list3.count("python") > 0:
-            print("Python is present.\n")
+            print("Python is present.")
         else:
-            print("Python is absent.\n")
-        
+            print("Python is absent.")
+
     elif choice == 5:
         condition = True
+
     else:
-        print("Enter a valid choice\n")
+        print("Invalid choice.")
