3d1ddf0c520e25103092c59466eb2f57b9110af0
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-11-15 11:20:33 +0530

    Update exp7.py

--- exp7.py
+++ exp7.py
@@ -2,45 +2,49 @@ class emp:
     def __init__ (self, id1, name1):
         self.id = id1
         self.name = name1
+        
     def display(self):
         print(self.id)
         print(self.name)
-    def setvalue(self, id1, name1):
+        
+    def setvalues(self,id1 ,name1):
         self.id = id1
         self.name = name1
         
-l1= list()
-    
-user=False
-while not user:
-	choice=int(input("\n\n1.Read an employee\n2.Modify the emloyee\n3.Display employees\n4.Delete employee\n5.Exit\nEnter choice: "))
-	if choice==1 :
-	    no_e=int(input("Enter the no of employees: "))
-	    for i in range(no_e):
-	        eid = int(input("Employee id: "))
-	        ename = input("Employee name: ")
-	        e1 = emp(eid, ename)
-	        l1.append(e1)
-	        
-	elif choice==2 :
-	    upid=int(input("Enter employee id to be updated: "))
-	    upname=input(f"Enter employee name to be updated at id {upid}: ")
-	    for q in l1:
-	        if q.id == upid:
-	            q.setvalue(upid, upname)
-	       
-	elif choice==3 :
-	    print("\n\n\t\tEMPLOYEES\t\t\n")
-	    for i in l1:
-	        print(f"\tId: {i.id} | Name: {i.name}")
+l1 = list()
 
-	elif choice==4 :
-	    did=int(input("Enter employee id to be deleted: "))
-	    for d in l1:
-	        if d.id == did:
-	            l1.remove(d)
-	    
-	elif choice==5 :
-		user=True
-	else :
-		print("Invalid choice")
+condition = False
+
+while not condition:
+    choice = int(input("\n\n1.Read employee \n2.Modify the employee \n3.Display the employees \n4.Delete an employee \n5.Exit \nEnter choice: "))
+    if choice == 1:
+        no_e = int(input("Enter the no of employees: "))
+        for i in range(no_e):
+            eid = int(input("Enter id: "))
+            ename = input("Enter name: ")
+            e1 = emp(eid, ename)
+            l1.append(e1)
+            
+    elif choice == 2:
+        upid = int(input("Enter the employee id: "))
+        upname = input(f"Enter the new name of employee at id {upid}: ")
+        for x in l1:
+            if x.id == upid:
+                x.setvalues(upid, upname)
+                
+    elif choice == 3:
+        print("\n\n\tEMPLOYEES\t\t\n")
+        for i in l1:
+            print(f"\tId: {i.id} | Name: {i.name}")
+            
+    elif choice == 4:
+        did = int(input("Enter the employee id to be deleted: "))
+        for d in l1:
+            if d.id == did:
+                l1.remove(d)
+                
+    elif choice == 5:
+        condition = True
+        
+    else:
+        print("Invalid choice")
